6fdffb150b4fd5a5f30eced2561e5290ef015ecd
Deepakab17 <deepaknab17@gmail.com>
2025-11-04 22:48:57 +0530

    add

--- test.py
+++ test.py
@@ -16,21 +16,33 @@
 #     print("available balane is = ", bank_balance-withdraw)
 # else:
 #     print("insufficient balance")
-subjects=["Maths", "Science", "Hindi", "English", "Social science"]
-marks=[]
-for subject in subjects:
-    score=int(input(f'enter marks in {subject} \n'))
-    marks.append(score)
+# subjects=["Maths", "Science", "Hindi", "English", "Social science"]
+# marks=[]
+# for subject in subjects:
+#     score=int(input(f'enter marks in {subject} \n'))
+#     marks.append(score)
 
-total_marks=sum(marks)
-percentage=(total_marks/(len(subjects)*100))*100
-if percentage >=65:
-    print("grade 'A'", percentage)
-elif percentage >=50 and percentage<65:
-    print("grade 'B'", percentage)
-elif percentage>=33 and percentage<50:
-    print("grade 'C'", percentage)
-else:
-    print("fail", percentage)
+# total_marks=sum(marks)
+# percentage=(total_marks/(len(subjects)*100))*100
+# if percentage >=65:
+#     print("grade 'A'", percentage)
+# elif percentage >=50 and percentage<65:
+#     print("grade 'B'", percentage)
+# elif percentage>=33 and percentage<50:
+#     print("grade 'C'", percentage)
+# else:
+#     print("fail", percentage)
 
+n=int(input("enter how many pairs you want to enter\n"))
+pairs=()
+for i in range(n):
+    key=input(f'Enter {i+1} key\n')
+    value=input(f'Enter {i+1} value\n')
+    if value.isdigit():
+        value=int(value)
     
+    pairs.append((key,value))
+
+dictonary=dict(pairs)
+print("tuple is")
+print("new dictionary is ", dictonary)
